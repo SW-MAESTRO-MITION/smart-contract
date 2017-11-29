78ddea7411dbec621c0981ca867ba1145056a53c
Sangwon Hong <qpakzk@gmail.com>
2017-11-29 10:21:11 +0900

    update Contract contract in smart_contract
    Signed-off-by: Sangwon Hong <qpakzk@gmail.com>

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,18 +1,45 @@
 pragma solidity ^0.4.18;
 
-contract Contract {
+contract Owned {
+    address public owner;
+
+    function Owned() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        owner = newOwner;
+    }
+
+    function getOwner() public constant returns (address) {
+        return owner;
+    }
+}
+
+contract Contract is Owned {
+  struct File {
+    string name;
     string date;
+    uint size;
+  }
 
-    event Instructor(
-      string date
-    );
+  mapping(string => File) private files;
+  event UploadFile(string hash, File file);
 
-    function set(string _date) public {
-        date = _date;
-        Instructor(_date);
-    }
+  function setFile(string date, string hash, string name, uint size) onlyOwner public {
+    require(size > 0);
 
-    function get() view public returns (string) {
-        return date;
-    }
+    File memory file = File(name, date, size);
+    files[hash] = file;
+    UploadFile(hash, file);
+  }
+
+  function getFile(string hash) view public returns (string, string, uint) {
+    return (files[hash].name, files[hash].date, files[hash].size);
+  }
 }
